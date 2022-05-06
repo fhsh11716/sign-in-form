5a2056bb93abb44a8a0112ecac4cb2cf18853f19
fhsh11716 <11035277@fhsh.tp.edu.tw>
2022-05-06 11:55:53 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -7,12 +7,20 @@ $(document).ready(function () {
 
   //TODO: 讓帳號密碼輸入時,版型維持
   $("#account").blur(function (e) {
-    if ($(e.target).val !== "") {
+    if ($(e.target).val() !== "") {
       $("#addr-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
     } else {
-      $("#addr-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
+      $("#addr-hint-text").removeClass("-translate-y-5 text-xs text-sky-500");
     }
   });
 
-  $("#password");
+  $("#password").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#password-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#password-hint-text").removeClass(
+        "-translate-y-5 text-xs text-sky-500"
+      );
+    }
+  });
 });
